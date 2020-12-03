9b334527f0f6f7fb1a9707457b35f4d52203a84c
Alexey-Kapitonov <dekaliostro@gmail.com>
2020-12-03 17:47:27 +0200

    task1-arr

--- js/task-1.js
+++ js/task-1.js
@@ -33,6 +33,13 @@ const logItems = function(array) {
 
 };
 
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
 //Task END
 
 console.log(" ");
